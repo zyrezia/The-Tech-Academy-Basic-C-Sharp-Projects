a2837755a948f47e1daaeae74d01ea134a647e34
zyrezia <mr_joseph_rodriguez@yahoo.com>
2025-09-19 13:23:49 -0400

    Add Method Class Assignment
    Added Method Class Assignment

---
+++ Method Class Assignment/Method_Class_Assignment/Method.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Method
+    {
+        public void DoProcess(int num1, int num2)
+        {
+            // Do a math operation on the first integer
+            int square = num1 * num1;
+
+            // Display the second integer to the screen
+            Console.WriteLine(num2);
+            Console.Read();
+        }
+    }
+}
---
+++ Method Class Assignment/Method_Class_Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Instantiate Method class
+            Method process = new Method();
+
+            int num1 = 10;
+            int num2 = 10;
+
+            // Calling a method in the Method class
+            process.DoProcess(num1, num2);
+
+        }
+    }
+}
